e6a84ed1d94845e1242dd81424a89f271d7c06f0
thesrs02 <thesrs02@gmail.com>
2023-11-08 04:27:42 +0000

    weights fix

--- utils/download_weights.py
+++ utils/download_weights.py
@@ -7,7 +7,11 @@ import sys
 from transformers.utils.hub import move_cache
 
 from controlnet_aux import HEDdetector
-from diffusers import ControlNetModel, StableDiffusionControlNetPipeline
+from diffusers import (
+    ControlNetModel,
+    StableDiffusionPipeline,
+    StableDiffusionControlNetPipeline,
+)
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 
 
@@ -31,25 +35,34 @@ os.makedirs(models_cache_dir, exist_ok=True)
 
 # Load to download automatically
 hed = HEDdetector.from_pretrained(controlnet_path)
+print("HED Downloaded")
 safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model)
+
+print("safety_checker Downloaded")
 
 controlnet = ControlNetModel.from_pretrained(
     controlnet_model,
     cache_dir=models_cache_dir,
 )
 
+print("controlnet Downloaded")
+
 for base_model in base_models:
     pipe = StableDiffusionControlNetPipeline.from_pretrained(
-        controlnet_model,
+        base_model,
         controlnet=controlnet,
         cache_dir=models_cache_dir,
     )
+
+print("all base_models Downloaded")
 
 for single_file_path in single_file_base_models:
     pipe = StableDiffusionControlNetPipeline.from_single_file(
         single_file_path,
         controlnet=controlnet,
     )
+    
+print("all single_file_models Downloaded")
 
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
     base_models[0],
@@ -58,4 +71,6 @@ pipe = StableDiffusionControlNetPipeline.from_pretrained(
 )
 
 for lora_weight_name in loras_list:
-    pipe = pipe.load_lora_weights("rehanhaider/sd-loras", weight_name=lora_weight_name)
+    pipe.load_lora_weights("rehanhaider/sd-loras", weight_name=lora_weight_name, cache_dir=models_cache_dir)
+    
+print("all LORAS Downloaded")
